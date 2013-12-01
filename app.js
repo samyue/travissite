a88cf761a0de88e17cbcfc7fad62d24bc6ae9600
sam <sam@sam-VirtualBox.(none)>
2013-12-01 15:16:54 +1100

    added if

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var http = require('http');
  
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n') // missing semi-colon will fail the build
+  res.end('Hello World\n'); // missing semi-colon will fail the build
 }).listen(1337, '127.0.0.1');
- 
+
+var x = 3;
+if(x = 3)
+    console.log('test') 
 console.log('Server running at http://127.0.0.1:1337/');
 
